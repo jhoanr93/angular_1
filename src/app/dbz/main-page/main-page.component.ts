fb830714aaf1e70e858b6c787a8a97896a86ef4f
jhoanr93 <jaratival@unal.edu.co>
2021-07-08 15:52:05 -0500

    Video 62 Output - EventEmitter

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
-interface Personaje{
-  nombre: string;
-  poder: number;
-}
+
 
 @Component({
   selector: 'app-main-page',
@@ -20,23 +18,16 @@ export class MainPageComponent  {
     poder: 1000
   }];
 
-  nuevo:Personaje = {
-    nombre: '',
-    poder: 0
+
+  nuevo: Personaje =  {
+    nombre:'Maestro Rochi',
+    poder: 1000
   }
 
-  agregar(){
-    if (this.nuevo.nombre.trim().length === 0){
-      return;
-    }
-
-    console.log( this.nuevo );
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder:0
-    }
+  addNewActor(arg: Personaje){
+    this.personajes.push( arg );
+  }
+  
   }
 
-}
+
